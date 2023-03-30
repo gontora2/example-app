68b1ee9093a5261e5302b196e6d48f89dcb58ee3
gontora <gontora0424@gmail.com>
2023-03-30 10:30:04 +0900

    動作チェック

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/sample/{id}',[\App\Http\Controllers\Sample\IndexController::class,'
 Route::get('/tweet',\App\Http\Controllers\Tweet\IndexController::class)->name('tweet.index'); 
 //Route::post('/tweet',\App\Http\Controllers\Tweet\IndexController::class)->name('tweet.index'); 
 Route::post('/tweet/create',\App\Http\Controllers\Tweet\CreateController::class)->name('tweet.create');
-//where()をつけることで入力値を制限させることが出来ます（下記の場合数字以外が渡された場合404になる）
+//where()をつけることで入力値を制限させることが出来ます！（下記の場合数字以外が渡された場合404になる）
 Route::get('/tweet/update/{tweetId}',\App\Http\Controllers\Tweet\Update\IndexController::class)->name('tweet.update.index')->where('tweetId','[0-9]+');
 Route::put('/tweet/update/{tweetId}',\App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put')->where('tweetId','[0-9]+');
 Route::delete('/tweet/delete/{tweetId}',\App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete')->where('tweetId','[0-9]+');
